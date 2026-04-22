5ea630e6717677b858b37a77053412b46dd4eca3
zhengzihting <ta95060689@gmail.com>
2026-04-22 13:28:07 +0800

    feat: third version-print the third member name

--- FirstClass5Team.java
+++ FirstClass5Team.java
@@ -3,5 +3,6 @@ public class FirstClass5Team{
     System.out.println("這是用來練習GitHub協作的程式!");
     System.out.println("\"學號：\"01357105\"\n姓名：\"黃星萍\"");
     System.out.println("\"學號：\"01357117\"\n姓名：\"郭芷瑄\"");
+    System.out.println("\"學號：\"01357118\"\n姓名：\"曾姿婷\"");
   }
 }
