aa0df87f21b0b7fe65532f04437513c570a2a20b
zx-zhixuan <zhixuan.41004@gmail.com>
2026-04-22 13:24:47 +0800

    feat: second version-print the second member name

--- FirstClass5Team.java
+++ FirstClass5Team.java
@@ -2,5 +2,6 @@ public class FirstClass5Team{
   public static void main(String[] args){
     System.out.println("這是用來練習GitHub協作的程式!");
     System.out.println("\"學號：\"01357105\"\n姓名：\"黃星萍\"");
+    System.out.println("\"學號：\"01357117\"\n姓名：\"郭芷瑄\"");
   }
 }
